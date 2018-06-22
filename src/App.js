958a362448ce2ec4c0bbeee1a155baa9325d8c58
Jeremy Chen <jeremy886@gmail.com>
2018-06-22 15:44:36 +1000

    "bind click listener to a button"

--- src/App.js
+++ src/App.js
@@ -3,14 +3,27 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+  state = {
+    persons: [
+      {name: "John", job: "principal"},
+      {name: "Jacob", job: "builder"},
+      {name: "Jobs", job: "CEO"}
+    ]
+  }
+
+  switchNameHandler = () => {
+    console.log("was clicked")
+  }
   render() {
     return (
       <div className="App">
         <h1>Welcome to React</h1>
         <p>This is a react app!</p>
-        <Person name="John" job="principal" />
-        <Person name="Jacob" job="builder">My hobby is racing.</Person>
-        <Person name="Jobs" job="CEO" />
+        <button onClick={this.switchNameHandler}>Switch Names</button>
+        <Person name={this.state.persons[0].name} job={this.state.persons[0].job} />
+        <Person name={this.state.persons[1].name} job={this.state.persons[1].job}>
+          My hobby is racing.</Person>
+        <Person name={this.state.persons[0].name} job={this.state.persons[2].job} />
       </div>
     );
   }
