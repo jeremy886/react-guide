8bfbf303de2a664386f48b8f6f1dfd1e6ff22496
Jeremy Chen <jeremy886@gmail.com>
2018-06-22 15:31:49 +1000

    "props"

--- src/App.js
+++ src/App.js
@@ -8,7 +8,9 @@ class App extends Component {
       <div className="App">
         <h1>Welcome to React</h1>
         <p>This is a react app!</p>
-        <Person />
+        <Person name="John" job="principal" />
+        <Person name="Jacob" job="builder">My hobby is racing.</Person>
+        <Person name="Jobs" job="CEO" />
       </div>
     );
   }
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,8 +1,13 @@
 import React from 'react';
 
 
-const person = () => {
-  return <p>I'm a Perosn!</p>
-}
+const person = (props) => {
+  return (
+    <div>
+      <p>I'm {props.name} and I'm a {props.job}. {props.children}</p>
+      <p>I drew the number {Math.floor(Math.random()*100)}!</p>
+    </div>
+  )
+};
 
 export default person;
